aa46ad1ef988822247d9c6dcce7ac6c6e543e91d
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-08-03 02:49:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,13 @@
 const config = {};
 config.packname = "NIMA BOT",
 config.author = "🛡️",
-config.botName = "NIMA BOT ", 
+config.botName = "NIMA BOT",
 config.botOwner = "NIMESHA",
 // 🔧 REPLACE BELOW - Add your number and choose any emoji
 config.ownerNumber = '94726800969',
 config.giphyApiKey = "qnls7ssQChTdTpjsKta2Ax2LMaGXz303tq",
 config.commandMode = "public",
 config.description = "This is a bot for managing group commands and automating tasks.",
-config.version = "999",
+config.version = "X",
 config.statusReactionEmoji = '🍆';
 module.exports = config;
-p
